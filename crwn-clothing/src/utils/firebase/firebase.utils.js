08b6b916b9904ee2a955a5d75a7998910a8d6bf4
Aaron Murillo <amurillo11@cnm.edu>
2023-05-09 23:34:48 -0600

    adding firestore to only the user to signin with their data.

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
   signInWithRedirect,
   signInWithPopup,
 } from "firebase/auth";
+import{
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBnR43XILDzAMSY9SEQWd04o6D1tAU-1Io",
@@ -22,5 +28,16 @@ const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 
 export const auth = getAuth();
-
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', 'userAuth.uid');
+  
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+}
